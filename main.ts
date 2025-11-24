1fd349e4debd4480153095320607811e0bd59539
tomas-quiros09 <tomas.quiros@panam.ed.cr>
2025-11-24 11:49:36 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -31,3 +31,8 @@ tiles.placeOnRandomTile(mySprite, sprites.dungeon.collectibleInsignia)
 scene.cameraFollowSprite(mySprite)
 info.startCountdown(50)
 mySprite.setBounceOnWall(true)
+game.onUpdate(function () {
+    if (Level_2 == true) {
+    	
+    }
+})
