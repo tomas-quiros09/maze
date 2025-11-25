8f012b76180425e55550c2e5a1fd3d9b63f87c49
lukewendel-crypto <luke.wendel@panam.ed.cr>
2025-11-25 13:22:59 -0600

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,38 +1,37 @@
 scene.onHitWall(SpriteKind.Player, function (sprite, location) {
     tiles.placeOnTile(null, tiles.getTileLocation(0, 0))
 })
-scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles29, function (sprite, location) {
-    game.over(true)
-    Level_2 = true
+info.onScore(1, function () {
+    Lvl_1 = 0
 })
-let Level_2 = false
-music.play(music.stringPlayable("C5 G G C5 G F E D ", 338), music.PlaybackMode.LoopingInBackground)
-let mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . 5 5 5 5 . . . . . 
-    . . . . . . . 5 5 5 5 . . . . . 
-    . . . . . . 5 5 5 5 5 5 . . . . 
-    . . . . . . 5 5 5 5 5 5 . . . . 
-    . . . . . 5 5 5 5 5 5 5 . . . . 
-    . . . . . 5 5 5 5 5 5 5 . . . . 
-    . . . . 5 5 5 5 5 5 5 5 . . . . 
-    . . . . 5 5 5 5 5 5 5 . . . . . 
-    . . . . 5 5 5 5 5 5 . . . . . . 
-    . . . . . 5 5 5 5 . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
-controller.moveSprite(mySprite, 100, 100)
-tiles.setTilemap(tilemap`level1`)
-tiles.placeOnRandomTile(mySprite, sprites.dungeon.collectibleInsignia)
-scene.cameraFollowSprite(mySprite)
-info.startCountdown(50)
-mySprite.setBounceOnWall(true)
-game.onUpdate(function () {
-    if (Level_2 == true) {
-    	
-    }
+scene.onOverlapTile(SpriteKind.Player, assets.tile`transparency16`, function (sprite, location) {
+	
 })
+let mySprite: Sprite = null
+let Lvl_1 = 0
+if (Lvl_1) {
+    music.play(music.stringPlayable("C5 G G C5 G F E D ", 338), music.PlaybackMode.LoopingInBackground)
+    mySprite = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 5 5 5 5 . . . . . 
+        . . . . . . . 5 5 5 5 . . . . . 
+        . . . . . . 5 5 5 5 5 5 . . . . 
+        . . . . . . 5 5 5 5 5 5 . . . . 
+        . . . . . 5 5 5 5 5 5 5 . . . . 
+        . . . . . 5 5 5 5 5 5 5 . . . . 
+        . . . . 5 5 5 5 5 5 5 5 . . . . 
+        . . . . 5 5 5 5 5 5 5 . . . . . 
+        . . . . 5 5 5 5 5 5 . . . . . . 
+        . . . . . 5 5 5 5 . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+    controller.moveSprite(mySprite, 100, 100)
+    tiles.setTilemap(tilemap`level1`)
+    tiles.placeOnRandomTile(mySprite, assets.tile`transparency16`)
+    scene.cameraFollowSprite(mySprite)
+    info.startCountdown(50)
+}
