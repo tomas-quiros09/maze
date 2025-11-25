bf5d1f57a317a74f9f4d3bbd82ac73d8c8b9640c
tomas-quiros09 <tomas.quiros@panam.ed.cr>
2025-11-25 14:01:02 -0600

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -2,36 +2,34 @@ scene.onHitWall(SpriteKind.Player, function (sprite, location) {
     tiles.placeOnTile(null, tiles.getTileLocation(0, 0))
 })
 info.onScore(1, function () {
-    Lvl_1 = 0
+    Lvl_2 = 0
 })
-scene.onOverlapTile(SpriteKind.Player, assets.tile`transparency16`, function (sprite, location) {
-	
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles29, function (sprite, location) {
+    game.gameOver(true)
+    game.setGameOverEffect(true, effects.confetti)
 })
-let mySprite: Sprite = null
-let Lvl_1 = 0
-if (Lvl_1) {
-    music.play(music.stringPlayable("C5 G G C5 G F E D ", 338), music.PlaybackMode.LoopingInBackground)
-    mySprite = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . 5 5 5 5 . . . . . 
-        . . . . . . . 5 5 5 5 . . . . . 
-        . . . . . . 5 5 5 5 5 5 . . . . 
-        . . . . . . 5 5 5 5 5 5 . . . . 
-        . . . . . 5 5 5 5 5 5 5 . . . . 
-        . . . . . 5 5 5 5 5 5 5 . . . . 
-        . . . . 5 5 5 5 5 5 5 5 . . . . 
-        . . . . 5 5 5 5 5 5 5 . . . . . 
-        . . . . 5 5 5 5 5 5 . . . . . . 
-        . . . . . 5 5 5 5 . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, SpriteKind.Player)
-    controller.moveSprite(mySprite, 100, 100)
-    tiles.setTilemap(tilemap`level1`)
-    tiles.placeOnRandomTile(mySprite, assets.tile`transparency16`)
-    scene.cameraFollowSprite(mySprite)
-    info.startCountdown(50)
-}
+let Lvl_2 = 0
+music.play(music.stringPlayable("C5 G G C5 G F E D ", 338), music.PlaybackMode.LoopingInBackground)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . 5 5 5 5 . . . . . 
+    . . . . . . . 5 5 5 5 . . . . . 
+    . . . . . . 5 5 5 5 5 5 . . . . 
+    . . . . . . 5 5 5 5 5 5 . . . . 
+    . . . . . 5 5 5 5 5 5 5 . . . . 
+    . . . . . 5 5 5 5 5 5 5 . . . . 
+    . . . . 5 5 5 5 5 5 5 5 . . . . 
+    . . . . 5 5 5 5 5 5 5 . . . . . 
+    . . . . 5 5 5 5 5 5 . . . . . . 
+    . . . . . 5 5 5 5 . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 100)
+tiles.setTilemap(tilemap`level1`)
+scene.cameraFollowSprite(mySprite)
+info.startCountdown(50)
+tiles.placeOnRandomTile(mySprite, sprites.dungeon.collectibleInsignia)
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -2,28 +2,68 @@
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
+            case "level_1":
+            case "level_1":return tiles.createTilemap(hex`100010000102020c0c1801020c0202180c0202030a0f0b0b0b0b090b0b0b0b0b0b0b0f17130b0b0b0b0b090b0b0b0b0f0b0b0b0d090b0b010c0c1b0f0b1a0707100b0b0d0a0b0b090b0b0b0b0b0d0f0b090b0b04090f0b0a0b0b0f0b0b0d0b0b0a0b0b04120b0b0a0b0b090f0b0d0b0f090f0b170a0b0b08100f090b0b0d0f0b120b0b0d090b0f0b0806090b0b0d0b0b0a0b0b040a0b0b0b0f0b090f0b0d0b0b090f0b0d0a0b0b0b0b0b090b0b0d0b0b090b0b04090b0b0c0c0c1b0b0f0b0b0f090b0b04120b0b0b0b0b0f0b0b0b0b0b090b0b170a0b0b0b0b0b090b0b0b0b0b0a0b1c04090f0b0b0b0f090b0b0f0b0b090f0b0408070719070608060719060608060705`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterWest0,sprites.dungeon.floorDark2,sprites.dungeon.greenOuterNorth1,sprites.dungeon.greenOuterEast1,sprites.dungeon.floorDark1,sprites.dungeon.floorDark5,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.purpleInnerNorthWest,sprites.dungeon.greenOuterWest2,sprites.dungeon.stairWest,sprites.dungeon.stairEast,sprites.dungeon.stairLarge,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.greenOuterEast2,sprites.dungeon.greenOuterNorth2,sprites.dungeon.greenOuterSouth2,sprites.dungeon.greenInnerNorthWest,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.stairLadder], TileScale.Sixteen);
+            case "level_0":
+            case "level_0":return tiles.createTilemap(hex`100010000102020c0c1201020c0202120c0202030a0e0b0b0b0b090b0b0b0b0b0b0b0e11000b0b0b0b0b090b0b0b0b0e0b0b0b0d090b0b010c0c150e0b1407070f0b0b0d0a0b0b090b0b0b0b0b0d0e0b090b0b04090e0b0a0b0b0e0b0b0d0b0b0a0b0b04100b0b0a0b0b090e0b0d0b0e090e0b110a0b0b080f0e090b0b0d0e0b100b0b0d090b0e0b0806090b0b0d0b0b0a0b0b040a0b0b0b0e0b090e0b0d0b0b090e0b0d0a0b0b0b0b0b090b0b0d0b0b090b0b04090b0b0c0c0c150b0e0b0b0e090b0b04100b0b0b0b0b0e0b0b0b0b0b090b0b110a0b0b0b0b0b090b0b0b0b0b0a0b0004090e0b0b0b0e090b0b0e0b0b090e0b0408070713070608060713060608060705`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterWest0,sprites.dungeon.floorDark2,sprites.dungeon.greenOuterNorth1,sprites.dungeon.greenOuterEast1,sprites.dungeon.floorDark5,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.greenOuterWest2,sprites.dungeon.greenOuterEast2,sprites.dungeon.greenOuterNorth2,sprites.dungeon.greenOuterSouth2,sprites.dungeon.greenInnerNorthWest,sprites.dungeon.greenInnerSouthEast,myTiles.transparency16], TileScale.Sixteen);
             case "level1":
-            case "level1":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`, [myTiles.transparency16,sprites.castle.tileGrass2], TileScale.Sixteen);
+            case "level1":return tiles.createTilemap(hex`1000100001090102010409040401060606060606050104060404050409010605050104060904040601040606060606060806040601010506090406040604050609010406040706060606060406090606060609060501050401040401060406010501040604060606060606060609060906010606010401040605010501040601060606060101040406050606060106040406010606060606060109050601060405060406060505050601040406040505040601060605040906010901060901090404090606060905060606060606060606060106040605040601040106090609040609040101010106010101060106040106050909010606060104010601010101060603`, img`
+2 2 2 . 2 2 2 2 2 2 . . . . . . 
+2 2 2 . 2 2 2 2 2 2 . 2 2 2 2 . 
+2 2 2 . 2 2 . . . . . . . . 2 . 
+2 2 2 . 2 2 . 2 . 2 2 . 2 2 2 . 
+2 . . . . . . 2 . 2 . . . . 2 . 
+2 2 2 2 2 2 2 2 . 2 . 2 2 2 2 . 
+2 . . . . . . . . 2 . 2 . 2 . . 
+2 . 2 2 . 2 2 2 2 2 . 2 . . . . 
+2 . 2 2 . 2 . . . 2 . 2 2 . 2 . 
+. . . . . 2 2 2 . 2 . 2 2 . 2 . 
+. 2 2 2 . 2 2 2 . 2 2 2 2 . 2 . 
+. 2 2 2 . 2 2 2 . 2 2 2 2 2 2 . 
+. . 2 2 . . . . . . . . . . 2 . 
+2 . 2 2 . 2 2 2 . 2 . 2 2 . 2 2 
+2 . . . . . . . . . . 2 2 . 2 2 
+2 2 . . . 2 2 2 . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.dungeon.collectibleInsignia,sprites.builtin.forestTiles29,sprites.castle.tileGrass1,sprites.castle.tileGrass3,sprites.castle.tilePath5,sprites.castle.tilePath1,sprites.builtin.forestTiles4,sprites.builtin.forestTiles1], TileScale.Sixteen);
         }
         return null;
     })
@@ -31,6 +71,8 @@ namespace myTiles {
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
             case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
         }
         return null;
     })
